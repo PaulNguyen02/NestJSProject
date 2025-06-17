3c954e54dc860ad6bb249bbd8944894e47482d59
Phúc Nguyễn <105428358+PaulNguyen02@users.noreply.github.com>
2025-06-17 16:26:10 +0700

    Update auth.controller.ts

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -30,12 +30,12 @@ export class AuthController {
     if (!user) {
       throw new UnauthorizedException('Sai tài khoản hoặc mật khẩu');
     }
-    const token = this.authService.login(user);
+    const token = await this.authService.login(user);
     const isAdmin = user.Roles === true;
     return {
       message: isAdmin ? 'Bạn là admin' : 'Bạn không phải là admin',
       role: isAdmin ? 'admin' : 'user',
-      access_token: (await token).access_token,
+      access_token: token.access_token,
     };
   }
 
